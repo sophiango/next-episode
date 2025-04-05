fa91edbfad5b5445bdd8b03e8ab5118d9efb5974
Sophia Ngo <sophiango@Sophias-MBP.localdomain>
2025-04-05 08:06:46 -0700

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,15 @@
 import './App.css'
+import HeaderNav from "./HeaderNav.tsx";
+import HeroHeader from "./HeroHeader.tsx";
 
 function App() {
     return (
         <>
-            <div>
-                <header className="bg-indigo-500">
-                    <nav className="container relative flex items-center justify-between px-6 py-8 mx-auto text-white">
-                        <a href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 md:w-10 md:h-10" fill="none"
-                                 viewBox="0 0 24 24"
-                                 stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"/>
-                            </svg>
-                        </a>
-
-                        <button className="md:hidden">
-                        <span v-show="!open">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7"/>
-                    </svg>
-                </span>
-
-                            <span v-show="open">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd"
-                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                              clip-rule="evenodd"/>
-                    </svg>
-                </span>
-                        </button>
-
-                        <div
-                            className="[open ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full'] absolute inset-x-0 z-30 w-full px-6 py-8 mt-4 space-y-6 transition-all duration-300 ease-in-out bg-indigo-600 top-16 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:space-y-0 md:-mx-6 md:flex md:items-center">
-                            <a href="#"
-                               className="block text-white transition-colors duration-300 md:px-6 hover:text-indigo-300">Home</a>
-                            <a href="#"
-                               className="block text-white transition-colors duration-300 md:px-6 hover:text-indigo-300"> About
-                            </a>
-                            <a href="#"
-                               className="block text-white transition-colors duration-300 md:px-6 hover:text-indigo-300"> Portfolio
-                            </a>
-                            <a href="#"
-                               className="block text-white transition-colors duration-300 md:px-6 hover:text-indigo-300"> Blogs
-                            </a>
-                            <a href="#"
-                               className="block text-white transition-colors duration-300 md:px-6 hover:text-indigo-300"> Contact
-                            </a>
-                        </div>
-                    </nav>
-                </header>
-
+            <div className="relative full-width">
+                <HeaderNav/>
                 <main>
                     <section>
-                        <div className="relative grid w-full bg-indigo-500 h-96 lg:h-[32rem] place-items-center">
-                            <div className="flex flex-col items-center mx-auto text-center">
-                                <h1 className="text-4xl font-semibold text-white uppercase md:text-6xl">Hero Header</h1>
-
-                                <p className="mt-6 text-lg leading-5 text-white">The best in town.</p>
-
-                                <a href="#about" className="mt-8 cursor-pointer animate-bounce">
-                                    <svg width="53" height="53" viewBox="0 0 53 53" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <circle cx="27" cy="26" r="18" stroke="white" stroke-width="2"/>
-                                        <path
-                                            d="M22.41 23.2875L27 27.8675L31.59 23.2875L33 24.6975L27 30.6975L21 24.6975L22.41 23.2875Z"
-                                            fill="white"/>
-                                    </svg>
-                                </a>
-                            </div>
-                        </div>
-
-                        <svg className="fill-indigo-500" viewBox="0 0 1440 57" fill="none"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1440 0H0V57C720 0 1440 57 1440 57V0Z"/>
-                        </svg>
+                        <HeroHeader/>
                     </section>
 
                     <section className="container px-6 py-8 mx-auto lg:py-16 " id="about">
